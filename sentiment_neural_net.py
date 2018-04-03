d18043ea2adb54e1c18e974e6445549fd25bfda5
franek <franek.szombara@gmail.com>
2018-04-03 11:00:22 +0200

    import bug fix

--- sentiment_neural_net.py
+++ sentiment_neural_net.py
@@ -1,5 +1,4 @@
 import neural_net_func as nnf
-import sentiment
 import pickle
 
 with open("train_and_test.pickle", 'rb') as file:
@@ -8,4 +7,4 @@ with open("train_and_test.pickle", 'rb') as file:
 layers_nodes, input_size, output_size = ([550,550,550,550,500,300,300], len(train_features[0]), 2)
 neural_net = nnf.construct_neural_net(layers_nodes, input_size, output_size)
 
-nnf.train_neural_network(train_features, train_labels, neural_net, save_path="./models")
+nnf.train_neural_network(train_features, train_labels, test_features, test_labels, neural_net)
